a824072d1c6758abe7b8217730872d91edfa0a81
Ben Steadman <steadmanben1@gmail.com>
2019-12-01 06:59:00 +0000

    [python] day 01 part 02

--- 01/solution.py
+++ 01/solution.py
@@ -1,19 +1,31 @@
 from math import floor
+from typing import Iterable, List
 
 
-def part_1(module_masses):
-    return sum(floor(m / 3) - 2 for m in module_masses)
+def calc_fuel(m: int) -> int:
+    return floor(m / 3) - 2
 
 
-def part_2():
-    pass
+def part_1(module_masses: List[int]) -> int:
+    return sum(calc_fuel(m) for m in module_masses)
+
+
+def gen_fuel(m: int) -> Iterable[int]:
+    m = calc_fuel(m)
+    while m > 0:
+        yield m
+        m = calc_fuel(m)
+
+
+def part_2(module_masses: List[int]) -> int:
+    return sum(sum(gen_fuel(m)) for m in module_masses)
 
 
 def main(puzzle_input_f):
     lines = [l.strip() for l in puzzle_input_f.readlines() if l]
     module_masses = [int(l) for l in lines]
     print("Part 1: ", part_1(module_masses))
-    print("Part 2: ", part_2())
+    print("Part 2: ", part_2(module_masses))
 
 
 if __name__ == "__main__":
