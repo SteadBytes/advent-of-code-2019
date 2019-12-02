06b47e05ad108bef618a737e1fdfab74d0908386
Ben Steadman <steadmanben1@gmail.com>
2019-12-02 07:27:27 +0000

    [python] day 02 part 02

--- 02/solution.py
+++ 02/solution.py
@@ -1,4 +1,5 @@
 import operator
+from itertools import product
 
 OPCODES = {
     1: operator.add,
@@ -7,10 +8,11 @@ OPCODES = {
 
 
 def run(prg):
-    prg = prg[:]
     ip = 0
-    while prg[ip] != 99:
+    while True:
         op = prg[ip]
+        if op == 99:
+            break
         p1, p2, p3 = prg[ip+1:ip + 4]
         x, y = prg[p1], prg[p2]
         prg[p3] = OPCODES[op](x, y)
@@ -23,15 +25,19 @@ def part_1(prg):
     return run(prg)[0]
 
 
-def part_2():
-    pass
+def part_2(prg):
+    for x2, x1 in product(range(100), range(100)):
+        _prg = prg[:]
+        _prg[1], _prg[2] = x1, x2
+        if run(_prg)[0] == 19690720:
+            return 100 * x1 + x2
 
 
 def main(puzzle_input_f):
     line = puzzle_input_f.read().strip()
     prg = [int(x) for x in line.split(",")]
     print("Part 1: ", part_1(prg[:]))  # copy to avoid mutation
-    print("Part 2: ", part_2())
+    print("Part 2: ", part_2(prg[:]))
 
 
 if __name__ == "__main__":
