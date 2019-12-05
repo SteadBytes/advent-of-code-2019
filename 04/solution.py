0b9f817d9198b609a41deee79e55bfc222d22cad
Ben Steadman <steadmanben1@gmail.com>
2019-12-05 06:52:10 +0000

    [python] day 04 part 02

--- 04/solution.py
+++ 04/solution.py
@@ -1,27 +1,38 @@
+from functools import partial
+from operator import eq, ge
+
+
 def monotonic_inc(s: str):
     return list(s) == sorted(s)
 
 
-def at_least_n_same(s: str, n: int):
-    return any(c >= n for c in map(s.count, s))
+def n_same(s: str, n: int, cmp):
+    return any(cmp(c, n) for c in map(s.count, s))
 
 
-def is_valid(pw):
-    return monotonic_inc(pw) and at_least_n_same(pw, 2)
+def is_valid(pw, cmp=ge):
+    return monotonic_inc(pw) and n_same(pw, 2, cmp)
+
+
+is_valid_p2 = partial(is_valid, cmp=eq)
+
+
+def count_valid(low, high, pred):
+    return sum(1 for i in range(low, high) if pred(str(i)))
 
 
 def part_1(low, high):
-    return sum(1 for i in range(low, high) if is_valid(str(i)))
+    return count_valid(low, high, is_valid)
 
 
-def part_2():
-    pass
+def part_2(low, high):
+    return count_valid(low, high, is_valid_p2)
 
 
 def main(puzzle_input_f):
     low, high = [int(x) for x in puzzle_input_f.readline().strip().split("-")]
     print("Part 1: ", part_1(low, high))
-    print("Part 2: ", part_2())
+    print("Part 2: ", part_2(low, high))
 
 
 if __name__ == "__main__":
--- 04/test_solution.py
+++ 04/test_solution.py
@@ -1,6 +1,6 @@
 import pytest
 
-from solution import is_valid
+from solution import is_valid, is_valid_p2
 
 
 @pytest.mark.parametrize(
@@ -8,3 +8,10 @@ from solution import is_valid
 )
 def test_is_valid(pw, valid):
     assert is_valid(pw) == valid
+
+
+@pytest.mark.parametrize(
+    "pw,valid", [("112233", True), ("123444", False), ("111122", True)]
+)
+def test_is_valid_p2(pw, valid):
+    assert is_valid_p2(pw) == valid
