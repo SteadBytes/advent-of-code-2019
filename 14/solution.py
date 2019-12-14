6ab012bbd012ca7c7086329c5146bd737dd103eb
Ben Steadman <steadmanben1@gmail.com>
2019-12-14 12:25:04 +0000

    [python] day 14 part 02

--- 14/solution.py
+++ 14/solution.py
@@ -1,30 +1,38 @@
+import math
 from collections import defaultdict
+from typing import Iterable, Dict, Tuple
+
+# TODO: Try this in clojure core.logic
 
 
-def parse_chemical(s: str):
+ChemicalQuant = Tuple[int, str]
+ReactionResult = ChemicalQuant
+ReactionRequirement = Tuple[ChemicalQuant, ...]
+Reaction = Tuple[ReactionResult, ReactionRequirement]
+ReactionsGraph = Dict[str, Reaction]
+
+
+def parse_chemical(s: str) -> ChemicalQuant:
     n, chemical = s.split(" ")
     return int(n), chemical
 
 
-def parse_reaction(s: str):
-    l, r = s.split(" => ")
-    output = parse_chemical(r)
+def parse_reaction(s: str) -> Reaction:
+    l, s = s.split(" => ")
+    output = parse_chemical(s)
     inputs = (parse_chemical(c) for c in l.split(", "))
     return tuple(inputs), output
 
 
-def parse_input(lines):
+def parse_input(lines: Iterable[str]) -> ReactionsGraph:
     return {
         out_chemical: (out_quant, inputs)
         for inputs, (out_quant, out_chemical) in (parse_reaction(l) for l in lines)
     }
 
 
-def part_1(lines):
-    # build mapping from outputs to input requirements
-    m = parse_input(lines)
-
-    required = {"FUEL": 1}
+def ore_required(g: ReactionsGraph, n_fuel: int) -> int:
+    required = {"FUEL": n_fuel}
     current = defaultdict(int)
 
     while True:
@@ -34,7 +42,7 @@ def part_1(lines):
             # done when no chemicals are still required
             return required["ORE"]
 
-        out_quant, inputs = m[next_chemical]
+        out_quant, inputs = g[next_chemical]
         n_consume, n_remaining = divmod(required[next_chemical], out_quant)
 
         if n_remaining != 0:
@@ -59,14 +67,39 @@ def part_1(lines):
             del current[in_chemical]
 
 
-def part_2():
-    pass
+def part_1(lines: Iterable[str]) -> int:
+    reaction_graph = parse_input(lines)
+    return ore_required(reaction_graph, 1)
+
+
+def part_2(lines: Iterable[str]) -> int:
+    """
+    Given x ore produces y fuel, 10**12 ore produces >= (f * 10**1) / x
+
+    `max_fuel` uses this approximation to find the maximum fuel that can be made
+    from 10**12 ore:
+    """
+    reaction_graph = parse_input(lines)
+
+    target = 10 ** 12
+
+    def max_fuel(fuel=1) -> int:
+        # ore for the *next* amount of fuel
+        ore = ore_required(reaction_graph, fuel + 1)
+        # base case: *next* amount of fuel is too much -> return previous
+        if ore > target:
+            return fuel
+        else:
+            # calculate next fuel value and recur
+            return max_fuel(math.floor((fuel + 1) * target / ore))
+
+    return max_fuel()
 
 
 def main(puzzle_input_f):
     lines = [l.strip() for l in puzzle_input_f.readlines() if l]
     print("Part 1: ", part_1(lines))
-    print("Part 2: ", part_2())
+    print("Part 2: ", part_2(lines))
 
 
 if __name__ == "__main__":
--- 14/test_solution.py
+++ 14/test_solution.py
@@ -1,6 +1,70 @@
 import pytest
 
-from solution import part_1, parse_reaction, parse_input
+from solution import parse_input, parse_reaction, part_1, part_2
+
+EXAMPLE_INPUTS = {
+    "A": [
+        "10 ORE => 10 A",
+        "1 ORE => 1 B",
+        "7 A, 1 B => 1 C",
+        "7 A, 1 C => 1 D",
+        "7 A, 1 D => 1 E",
+        "7 A, 1 E => 1 FUEL",
+    ],
+    "B": [
+        "9 ORE => 2 A",
+        "8 ORE => 3 B",
+        "7 ORE => 5 C",
+        "3 A, 4 B => 1 AB",
+        "5 B, 7 C => 1 BC",
+        "4 C, 1 A => 1 CA",
+        "2 AB, 3 BC, 4 CA => 1 FUEL",
+    ],
+    "C": [
+        "157 ORE => 5 NZVS",
+        "165 ORE => 6 DCFZ",
+        "44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL",
+        "12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ",
+        "179 ORE => 7 PSHF",
+        "177 ORE => 5 HKGWZ",
+        "7 DCFZ, 7 PSHF => 2 XJWVT",
+        "165 ORE => 2 GPVTF",
+        "3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT",
+    ],
+    "D": [
+        "2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG",
+        "17 NVRVD, 3 JNWZP => 8 VPVL",
+        "53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL",
+        "22 VJHF, 37 MNCFX => 5 FWMGM",
+        "139 ORE => 4 NVRVD",
+        "144 ORE => 7 JNWZP",
+        "5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC",
+        "5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV",
+        "145 ORE => 6 MNCFX",
+        "1 NVRVD => 8 CXFTF",
+        "1 VJHF, 6 MNCFX => 4 RFSQX",
+        "176 ORE => 6 VJHF",
+    ],
+    "E": [
+        "171 ORE => 8 CNZTR",
+        "7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL",
+        "114 ORE => 4 BHXH",
+        "14 VRPVC => 6 BMBT",
+        "6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL",
+        "6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT",
+        "15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW",
+        "13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW",
+        "5 BMBT => 4 WPTQ",
+        "189 ORE => 9 KTJDG",
+        "1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP",
+        "12 VRPVC, 27 CNZTR => 2 XDBXC",
+        "15 KTJDG, 12 BHXH => 5 XCVML",
+        "3 BHXH, 2 VRPVC => 7 MZWV",
+        "121 ORE => 7 VRPVC",
+        "7 XCVML => 6 RJRHP",
+        "5 BHXH, 4 VRPVC => 5 LTCX",
+    ],
+}
 
 
 @pytest.mark.parametrize(
@@ -24,14 +88,7 @@ def test_parse_reaction(reaction, io_chemicals):
     "input_lines,reaction_graph",
     [
         (
-            [
-                "10 ORE => 10 A",
-                "1 ORE => 1 B",
-                "7 A, 1 B => 1 C",
-                "7 A, 1 C => 1 D",
-                "7 A, 1 D => 1 E",
-                "7 A, 1 E => 1 FUEL",
-            ],
+            EXAMPLE_INPUTS["A"],
             {
                 "FUEL": (1, ((7, "A"), (1, "E"))),
                 "E": (1, ((7, "A"), (1, "D"))),
@@ -49,84 +106,19 @@ def test_parse_input(input_lines, reaction_graph):
 
 @pytest.mark.parametrize(
     "input_lines,required_ore",
-    [
-        (
-            [
-                "10 ORE => 10 A",
-                "1 ORE => 1 B",
-                "7 A, 1 B => 1 C",
-                "7 A, 1 C => 1 D",
-                "7 A, 1 D => 1 E",
-                "7 A, 1 E => 1 FUEL",
-            ],
-            31,
-        ),
-        (
-            [
-                "9 ORE => 2 A",
-                "8 ORE => 3 B",
-                "7 ORE => 5 C",
-                "3 A, 4 B => 1 AB",
-                "5 B, 7 C => 1 BC",
-                "4 C, 1 A => 1 CA",
-                "2 AB, 3 BC, 4 CA => 1 FUEL",
-            ],
-            165,
-        ),
-        (
-            [
-                "157 ORE => 5 NZVS",
-                "165 ORE => 6 DCFZ",
-                "44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL",
-                "12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ",
-                "179 ORE => 7 PSHF",
-                "177 ORE => 5 HKGWZ",
-                "7 DCFZ, 7 PSHF => 2 XJWVT",
-                "165 ORE => 2 GPVTF",
-                "3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT",
-            ],
-            13312,
-        ),
-        (
-            [
-                "2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG",
-                "17 NVRVD, 3 JNWZP => 8 VPVL",
-                "53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL",
-                "22 VJHF, 37 MNCFX => 5 FWMGM",
-                "139 ORE => 4 NVRVD",
-                "144 ORE => 7 JNWZP",
-                "5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC",
-                "5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV",
-                "145 ORE => 6 MNCFX",
-                "1 NVRVD => 8 CXFTF",
-                "1 VJHF, 6 MNCFX => 4 RFSQX",
-                "176 ORE => 6 VJHF",
-            ],
-            180697,
-        ),
-        (
-            [
-                "171 ORE => 8 CNZTR",
-                "7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL",
-                "114 ORE => 4 BHXH",
-                "14 VRPVC => 6 BMBT",
-                "6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL",
-                "6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT",
-                "15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW",
-                "13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW",
-                "5 BMBT => 4 WPTQ",
-                "189 ORE => 9 KTJDG",
-                "1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP",
-                "12 VRPVC, 27 CNZTR => 2 XDBXC",
-                "15 KTJDG, 12 BHXH => 5 XCVML",
-                "3 BHXH, 2 VRPVC => 7 MZWV",
-                "121 ORE => 7 VRPVC",
-                "7 XCVML => 6 RJRHP",
-                "5 BHXH, 4 VRPVC => 5 LTCX",
-            ],
-            2210736,
-        ),
-    ],
+    list(zip(EXAMPLE_INPUTS.values(), (31, 165, 13312, 180697, 2210736))),
 )
 def test_part_1(input_lines, required_ore):
     assert part_1(input_lines) == required_ore
+
+
+@pytest.mark.parametrize(
+    "input_lines,max_fuel",
+    [
+        (EXAMPLE_INPUTS["C"], 82892753),
+        (EXAMPLE_INPUTS["D"], 5586022),
+        (EXAMPLE_INPUTS["E"], 460664),
+    ],
+)
+def test_part_2(input_lines, max_fuel):
+    assert part_2(input_lines) == max_fuel
