bef4dff30f4c416e2d263c7bec4f3c1d5a65a8da
Ben Steadman <steadmanben1@gmail.com>
2019-12-20 07:14:33 +0000

    [python] day 18 part 02 initial

--- 18/solution.py
+++ 18/solution.py
@@ -3,6 +3,7 @@ from operator import add
 from heapq import heappop, heappush
 
 ENTRANCE = "@"
+WALL = "#"
 
 # cannot move diagonally
 Y_DIRS = [-1, 0, 1, 0]
@@ -50,7 +51,7 @@ def part_1(lines):
             for diff in zip(Y_DIRS, X_DIRS):
                 next_p = tuple(map(add, p, diff))
                 pos_id = grid[next_p[0]][next_p[1]]
-                if pos_id != "#":
+                if pos_id != WALL:
                     q.append(
                         (
                             next_p,
@@ -81,17 +82,119 @@ def part_1(lines):
             # next node has the same number of keys in the path
             # and is not on the current path
             if len(doors_ - doors) == 0 and pos_id not in doors:
-                heappush(q, (dist + dist_, (v, doors | frozenset(v))))
+                heappush(q, (dist + dist_, (pos_id, doors | frozenset(pos_id))))
 
 
-def part_2():
-    pass
+def map_keys_and_entrances(grid, new_entrances):
+    new_entrances = new_entrances[:]
+    d = dict()
+    for y, row in enumerate(grid):
+        for x, val in enumerate(row):
+            if val.isupper() or val in ".#":
+                continue
+            if val == ENTRANCE:
+                d[new_entrances.pop()] = (y, x)
+            else:
+                d[val] = (y, x)
+    assert not new_entrances
+    return d
+
+
+def correct_grid_entrances(grid, entrance_location=(40, 40)):
+    # ...    @.@
+    # .@. -> .@.
+    # ...    @.@
+
+    diffs = ((-1, -1), (-1, 1), (1, -1), (1, 1))
+    new_entrance_locations = [tuple(map(add, entrance_location, d)) for d in diffs]
+    for (y, x) in new_entrance_locations:
+        grid[y][x] = ENTRANCE
+
+    # @.@    @#@
+    # .@. -> ###
+    # @.@    @#@
+
+    diffs = ((-1, 0), (0, -1), (0, 1), (1, 0))
+    new_walls = [tuple(map(add, entrance_location, d)) for d in diffs] + [
+        entrance_location
+    ]
+    for y, x in new_walls:
+        grid[y][x] = WALL
+    return grid
+
+
+def part_2(lines):
+    grid = correct_grid_entrances([list(l) for l in lines])
+    new_entrances = ["1", "2", "3", "4"]
+    d = map_keys_and_entrances(grid, new_entrances)
+
+    assert ENTRANCE not in d  # should be 1, 2, 3, 4
+    assert all(l in d for l in new_entrances)  # all entrances mapped
+
+    n_keys = len(d.keys()) - len(new_entrances)  # account for entrances
+
+    def bfs(pos):
+        q = [(pos, 0, ())]
+        seen, paths = set(), dict()
+        while q:
+            p, dist, doors = q.pop(0)
+            if p in seen:
+                continue
+            seen.add(p)
+
+            pos_id = grid[p[0]][p[1]]
+            # this is a key
+            if pos_id in d and p != pos and pos not in new_entrances:
+                paths[pos_id] = (dist, frozenset(doors))
+
+            for diff in zip(Y_DIRS, X_DIRS):
+                next_p = tuple(map(add, p, diff))
+                pos_id = grid[next_p[0]][next_p[1]]
+                if pos_id != WALL:
+                    doors_ = (
+                        doors + (pos_id.lower(),) if pos_id.isupper() else doors
+                    )
+                    q.append((next_p, dist + 1, doors_))
+        return paths
+
+    g = defaultdict(dict)
+    for pos_id, pos in d.items():
+        for k, v in bfs(pos).items():
+            g[pos_id][k] = v
+
+    # (distance, (robot positions, doors crossed so far))
+    q = [(0, (tuple(new_entrances), frozenset()))]
+    distances = dict()
+
+    while q:
+        dist, n = heappop(q)
+        if n in distances:
+            continue
+        distances[n] = dist
+        nodes, doors = n
+        if len(doors) == n_keys:
+            return dist  # done!
+        for i, start in enumerate(nodes):
+            for pos_id, (dist_, doors_) in g[start].items():
+                # next node has the same number of keys in the path
+                # and is not on the current path
+                if len(doors_ - doors) == 0 and pos_id not in doors:
+                    heappush(
+                        q,
+                        (
+                            dist + dist_,
+                            (
+                                nodes[:i] + (pos_id,) + nodes[i + 1 :],
+                                doors | frozenset(pos_id),
+                            ),
+                        ),
+                    )
 
 
 def main(puzzle_input_f):
     lines = [l.strip() for l in puzzle_input_f.readlines() if l]
     print("Part 1: ", part_1(lines))
-    print("Part 2: ", part_2())
+    print("Part 2: ", part_2(lines))
 
 
 if __name__ == "__main__":
