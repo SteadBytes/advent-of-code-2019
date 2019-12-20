522d559d6d0eac88d8462030fe9a09e53887abb4
Ben Steadman <steadmanben1@gmail.com>
2019-12-20 07:29:04 +0000

    [python] day 18 refactoring
    [python] day 18 refactor BFS graph building
    
    [python] day 18 refactor Dijkstra's search implementation
    
    [python] Improve documentation
    
    [python] day 18 replace ad-hoc tuple with Coord NamedTuple
    
    [python] day 18 type hints

--- 18/solution.py
+++ 18/solution.py
@@ -1,169 +1,133 @@
+"""
+There is a *unique* path from any pair of keys and starting point in the grid.
+This solution uses a BFS to build a graph where nodes are entrances or keys
+(referred to as Points of Interest) and edges represent the distance between nodes
+and the keys required to traverse that path.
+
+Then, perform Dijkstras shortest path algorithm over the graph where nodes (POIs)
+are represented by (distance, starting point, keys gathered). The search halts
+when keys gathered == number of keys on the grid.
+"""
 from collections import defaultdict
-from operator import add
 from heapq import heappop, heappush
+from itertools import count
+from typing import DefaultDict, Dict, FrozenSet, List, NamedTuple, Tuple
+
+
+class Coord(NamedTuple):
+    x: int
+    y: int
+
+    def __add__(self, other):
+        assert len(other) == len(self)
+        return self.__class__(*(x + y for x, y in zip(self, other)))
+
+    def __sub__(self, other):
+        assert len(other) == len(self)
+        return self.__class__(*(x - y for x, y in zip(self, other)))
+
 
 ENTRANCE = "@"
 WALL = "#"
 
-# cannot move diagonally
-Y_DIRS = [-1, 0, 1, 0]
-X_DIRS = [0, 1, 0, -1]
+# Cannot move diagonally
+MOVE_DIRS = [Coord(x=0, y=-1), Coord(x=1, y=0), Coord(x=0, y=1), Coord(x=-1, y=0)]
+
+Grid = List[List[str]]
+POIMap = Dict[str, Coord]
+POIGraph = DefaultDict[str, Dict[str, Tuple[int, FrozenSet[str]]]]
 
 
-def map_keys_from_entrance(grid):
-    d = dict()
-    for y, row in enumerate(grid):
-        for x, val in enumerate(row):
-            if val.islower() or val == ENTRANCE:
-                d[val] = (y, x)
-    return d
-
-
-def part_1(lines):
+def map_points_of_interest(grid: Grid) -> Tuple[POIMap, List[str]]:
     """
-    There is a *unique* path from any pair of keys and starting point in the grid.
-    Use a BFS to build a graph where nodes are starting points (entrance or key)
-    and edges representing distance and doors crossed along that path.
+    Returns a map from points of interest (POIs) to coordinates and a
+    list of entrance names.
 
-    Then, perform Dijkstras shortest path algorithm over the graph where nodes
-    (entrance or key) are represented by (distance, starting point, keys gathered).
-    The search halts when keys gathered == number of keys on the grid.
+    Where POIs are:
+        - Entrances
+        - Keys
+
+    Entrances are named sequentially from 1 in the order they are found. For a
+    grid containing a single entrance, the map will contain an entry for an
+    entrance `"0"` and the list of entrance names will be `["0"]`
+
     """
-    grid = [list(l) for l in lines]
-    d = map_keys_from_entrance(grid)
-    entrance_location = d[ENTRANCE]
-    n_keys = len(d.keys()) - 1  # account for entrance
-
-    def bfs(pos):
-        q = [(pos, 0, ())]
-        seen, paths = set(), dict()
-        while q:
-            p, dist, doors = q.pop(0)
-            if p in seen:
-                continue
-            seen.add(p)
-
-            pos_id = grid[p[0]][p[1]]
-            # this is a key
-            if pos_id in d and p not in (pos, entrance_location):
-                paths[pos_id] = (dist, frozenset(doors))
-
-            for diff in zip(Y_DIRS, X_DIRS):
-                next_p = tuple(map(add, p, diff))
-                pos_id = grid[next_p[0]][next_p[1]]
-                if pos_id != WALL:
-                    q.append(
-                        (
-                            next_p,
-                            dist + 1,
-                            doors + (pos_id.lower(),) if pos_id.isupper() else doors,
-                        )
-                    )
-        return paths
-
-    g = defaultdict(dict)
-    for pos_id, pos in d.items():
-        for k, v in bfs(pos).items():
-            g[pos_id][k] = v
-
-    # (distance, (starting point, doors crossed so far))
-    q = [(0, (ENTRANCE, frozenset()))]
-    distances = dict()
-
-    while q:
-        dist, n = heappop(q)
-        if n in distances:
-            continue
-        distances[n] = dist
-        start, doors = n
-        if len(doors) == n_keys:
-            return dist  # done!
-        for pos_id, (dist_, doors_) in g[start].items():
-            # next node has the same number of keys in the path
-            # and is not on the current path
-            if len(doors_ - doors) == 0 and pos_id not in doors:
-                heappush(q, (dist + dist_, (pos_id, doors | frozenset(pos_id))))
-
-
-def map_keys_and_entrances(grid, new_entrances):
-    new_entrances = new_entrances[:]
-    d = dict()
+    entrance_names = map(str, count())
+    d, entrances = dict(), []
     for y, row in enumerate(grid):
         for x, val in enumerate(row):
             if val.isupper() or val in ".#":
                 continue
             if val == ENTRANCE:
-                d[new_entrances.pop()] = (y, x)
+                entrance = next(entrance_names)
+                entrances.append(entrance)
+                d[entrance] = Coord(x, y)
             else:
-                d[val] = (y, x)
-    assert not new_entrances
-    return d
+                d[val] = Coord(x, y)
+    return d, entrances
 
 
-def correct_grid_entrances(grid, entrance_location=(40, 40)):
-    # ...    @.@
-    # .@. -> .@.
-    # ...    @.@
+def build_graph(grid: Grid, poi_map: POIMap, entrances: List[str]) -> POIGraph:
+    """
+    Returns a graph where nodes are starting POIs and edges are the associated
+    distance and *keys* crossed on the path to another key.
 
-    diffs = ((-1, -1), (-1, 1), (1, -1), (1, 1))
-    new_entrance_locations = [tuple(map(add, entrance_location, d)) for d in diffs]
-    for (y, x) in new_entrance_locations:
-        grid[y][x] = ENTRANCE
+    i.e. `{"0": {"a": (10, frozenset({'b', 'd'}))}...}` shows that the path from
+    the entrance `"0"` to the key `"a"` has a distance of `10` and crosses the
+    locations of keys `b` and `d`.
+    """
 
-    # @.@    @#@
-    # .@. -> ###
-    # @.@    @#@
-
-    diffs = ((-1, 0), (0, -1), (0, 1), (1, 0))
-    new_walls = [tuple(map(add, entrance_location, d)) for d in diffs] + [
-        entrance_location
-    ]
-    for y, x in new_walls:
-        grid[y][x] = WALL
-    return grid
-
-
-def part_2(lines):
-    grid = correct_grid_entrances([list(l) for l in lines])
-    new_entrances = ["1", "2", "3", "4"]
-    d = map_keys_and_entrances(grid, new_entrances)
-
-    assert ENTRANCE not in d  # should be 1, 2, 3, 4
-    assert all(l in d for l in new_entrances)  # all entrances mapped
-
-    n_keys = len(d.keys()) - len(new_entrances)  # account for entrances
-
-    def bfs(pos):
+    def bfs(pos: Coord):
         q = [(pos, 0, ())]
         seen, paths = set(), dict()
         while q:
-            p, dist, doors = q.pop(0)
+            p, dist, required_keys = q.pop(0)
             if p in seen:
                 continue
             seen.add(p)
+            pos_id = grid[p.y][p.x]
+            # An unseen key or door
+            if p != pos and pos_id in poi_map and pos_id not in entrances:
+                paths[pos_id] = (dist, frozenset(required_keys))
 
-            pos_id = grid[p[0]][p[1]]
-            # this is a key
-            if pos_id in d and p != pos and pos not in new_entrances:
-                paths[pos_id] = (dist, frozenset(doors))
-
-            for diff in zip(Y_DIRS, X_DIRS):
-                next_p = tuple(map(add, p, diff))
-                pos_id = grid[next_p[0]][next_p[1]]
+            for diff in MOVE_DIRS:
+                next_p = p + diff
+                pos_id = grid[next_p.y][next_p.x]
                 if pos_id != WALL:
-                    doors_ = (
-                        doors + (pos_id.lower(),) if pos_id.isupper() else doors
+                    q.append(
+                        (
+                            next_p,
+                            dist + 1,
+                            required_keys + (pos_id.lower(),)
+                            if pos_id.isupper()  # Position is a door -> key is required
+                            else required_keys,
+                        )
                     )
-                    q.append((next_p, dist + 1, doors_))
         return paths
 
     g = defaultdict(dict)
-    for pos_id, pos in d.items():
+    for pos_id, pos in poi_map.items():
         for k, v in bfs(pos).items():
             g[pos_id][k] = v
 
-    # (distance, (robot positions, doors crossed so far))
-    q = [(0, (tuple(new_entrances), frozenset()))]
+    return g
+
+
+def find_shortest_path_distance(g: POIGraph, entrances: List[str], n_keys: int) -> int:
+    """
+    Perform Dijkstra's search algorithm to find the shorted path from the entrance(s)
+    to collect all the keys on the grid.
+
+    Search nodes are represented as tuples of distance, robot positions and keys
+    gathered so far i.e. `(10, ("0", "1", "2", "t"), frozenset("t"))` means the
+    current path has a distance of `10`, 4 robots (3 of which are at entrances
+    and one is at key `"t"`) and the path has so far collected only key `"t"`.
+
+    The search halts when the number of keys gathered so far is equal to the total
+    number of keys on the grid.
+    """
+    # (distance, (robot positions, keys gathered so far))
+    q = [(0, (tuple(entrances), frozenset()))]
     distances = dict()
 
     while q:
@@ -171,24 +135,69 @@ def part_2(lines):
         if n in distances:
             continue
         distances[n] = dist
-        nodes, doors = n
-        if len(doors) == n_keys:
-            return dist  # done!
+        nodes, keys = n
+        if len(keys) == n_keys:
+            return dist  # Found all keys -> done!
         for i, start in enumerate(nodes):
-            for pos_id, (dist_, doors_) in g[start].items():
-                # next node has the same number of keys in the path
-                # and is not on the current path
-                if len(doors_ - doors) == 0 and pos_id not in doors:
+            for pos_id, (dist_, keys_) in g[start].items():
+                # next node has the same number of keys in the path (i.e. it's)
+                # not a *worse* path and is not already on the current path
+                if len(keys_ - keys) == 0 and pos_id not in keys:
                     heappush(
                         q,
                         (
                             dist + dist_,
                             (
                                 nodes[:i] + (pos_id,) + nodes[i + 1 :],
-                                doors | frozenset(pos_id),
+                                keys | frozenset(pos_id),
                             ),
                         ),
                     )
+
+
+def part_1(lines: List[str]):
+    grid = [list(l) for l in lines]
+    poi_map, entrances = map_points_of_interest(grid)
+    assert len(entrances) == 1
+    n_keys = len(poi_map.keys()) - 1  # account for entrance
+    g = build_graph(grid, poi_map, entrances)
+    return find_shortest_path_distance(g, entrances, n_keys)
+
+
+def correct_grid_entrances(
+    grid: Grid, entrance_location: Coord = Coord(40, 40)
+) -> Grid:
+    """
+    Adjust the center entrance location of the grid according the 4 entrance
+    specification in part two of the puzzle.
+    """
+    # ...    @.@
+    # .@. -> .@.
+    # ...    @.@
+
+    diffs = (Coord(-1, -1), Coord(1, -1), Coord(-1, 1), Coord(1, 1))
+    new_entrance_locations = [entrance_location + d for d in diffs]
+    for p in new_entrance_locations:
+        grid[p.y][p.x] = ENTRANCE
+
+    # @.@    @#@
+    # .@. -> ###
+    # @.@    @#@
+
+    diffs = (Coord(0, -1), Coord(-1, 0), Coord(1, 0), Coord(0, 1))
+    new_walls = [entrance_location + d for d in diffs] + [entrance_location]
+    for p in new_walls:
+        grid[p.y][p.x] = WALL
+    return grid
+
+
+def part_2(lines: List[str]):
+    grid = correct_grid_entrances([list(l) for l in lines])
+    poi_map, entrances = map_points_of_interest(grid)
+    assert len(entrances) == 4  # 0, 1, 2, 3
+    n_keys = len(poi_map.keys()) - len(entrances)  # account for entrances
+    g = build_graph(grid, poi_map, entrances)
+    return find_shortest_path_distance(g, entrances, n_keys)
 
 
 def main(puzzle_input_f):
