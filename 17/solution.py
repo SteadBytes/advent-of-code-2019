1472f09121769ba8313add252c237fdf722a4038
Ben Steadman <steadmanben1@gmail.com>
2019-12-25 07:28:15 +0000

    [python] Initial day 17 part 02 (finally)
    Needs some serious cleanup!

--- 17/solution.py
+++ 17/solution.py
@@ -1,7 +1,9 @@
 import asyncio
+import re
 from enum import Enum
+from functools import reduce
 from itertools import takewhile
-from typing import Dict, Iterable, List, NamedTuple, TypeVar
+from typing import Dict, Iterable, List, NamedTuple, Tuple, TypeVar
 
 import intcode
 
@@ -28,10 +30,31 @@ class Direction(Enum):
 
 MOVE_DIRECTIONS = {
     Direction.NORTH: Coord(0, -1),
-    Direction.EAST: Coord(0, 1),
-    Direction.SOUTH: Coord(-1, 0),
-    Direction.WEST: Coord(1, 0),
+    Direction.EAST: Coord(1, 0),
+    Direction.SOUTH: Coord(0, 1),
+    Direction.WEST: Coord(-1, 0),
 }
+
+
+class Turn(Enum):
+    LEFT = "L"
+    RIGHT = "R"
+
+
+LEFT_TURN_DIRECTIONS = {
+    Direction.NORTH: Direction.WEST,
+    Direction.EAST: Direction.NORTH,
+    Direction.SOUTH: Direction.EAST,
+    Direction.WEST: Direction.SOUTH,
+}
+
+RIGHT_TURN_DIRECTIONS = {v: k for k, v in LEFT_TURN_DIRECTIONS.items()}
+
+MOVEMENT_FUNCTION_CHAR_LIMIT = 20  # Not including newline
+
+MOVEMENT_FUNCTION_RE = (
+    r"^(?P<A>.{1,20})\1*(?P<B>.{1,20})(?:\1|\2)*(?P<C>.{1,20})(?:\1|\2|\3)*$"
+)
 
 T = TypeVar("T")
 Grid = Iterable[List[str]]
@@ -53,6 +76,12 @@ def split_iter(iterable: Iterable[T], sep: T) -> Iterable[List[T]]:
         if not split:
             break
         yield split
+
+
+def pairwise_exclusive(iterable):
+    """s -> (s0,s1), (s2,s3), (s4, s5.."""
+    it = iter(iterable)
+    return zip(it, it)
 
 
 def grid_to_map(grid: Grid) -> ScaffoldMap:
@@ -87,13 +116,91 @@ def sum_alignment_parameters(m: ScaffoldMap) -> int:
 
 def part_1(prg: intcode.Program):
     mem = intcode.prg_to_memory(prg)
-    map = build_map(mem)
-    return sum_alignment_parameters(map)
+    m = build_map(mem)
+    return sum_alignment_parameters(m)
+
+
+def find_robot(m: ScaffoldMap) -> Tuple[Coord, Direction]:
+    for loc, val in m.items():
+        if val in "^v<>":
+            return (loc, Direction(val))
+
+
+def scaffold_route(start_loc: Coord, start_dir: Direction, m: ScaffoldMap):
+    # TODO: PLEASE IMPROVE THIS
+    current_loc, current_dir = start_loc, start_dir
+    forward_count = 0
+    while True:
+        # Try move forward
+        next_loc = current_loc + MOVE_DIRECTIONS[current_dir]
+        if m.get(next_loc) == "#":
+            current_loc = next_loc
+            forward_count += 1
+        else:
+            # Try left turn
+            next_dir = LEFT_TURN_DIRECTIONS[current_dir]
+            next_loc = current_loc + MOVE_DIRECTIONS[next_dir]
+            if m.get(next_loc) == "#":
+                current_dir = next_dir
+                if forward_count:
+                    yield forward_count
+                    forward_count = 0
+                yield Turn.LEFT
+            else:
+                # Try right turn
+                next_dir = RIGHT_TURN_DIRECTIONS[current_dir]
+                next_loc = current_loc + MOVE_DIRECTIONS[next_dir]
+                if m.get(next_loc) == "#":
+                    if forward_count:
+                        yield forward_count
+                        forward_count = 0
+                    current_dir = next_dir
+                    yield Turn.RIGHT
+                else:
+                    if forward_count:
+                        yield forward_count
+                    return
+
+
+async def run_movement_program(mem: intcode.Memory, main_routine, movement_funcs):
+    mem[0] = 2
+    inqueue = asyncio.Queue()
+    for c in main_routine + "\n":
+        await inqueue.put(ord(c))
+    for k, pattern in movement_funcs.items():
+        for c in pattern + "\n":
+            await inqueue.put(ord(c))
+    await inqueue.put(ord("n"))
+    await inqueue.put(ord("\n"))
+
+    collector = output_collector()
+    await asyncio.create_task(intcode.execute(mem, inqueue.get, collector))
+    return collector.output
 
 
 def part_2(prg: intcode.Program):
+    mem = intcode.prg_to_memory(prg)
+    m = build_map(mem)
+    robot_loc, robot_dir = find_robot(m)
+    route = scaffold_route(robot_loc, robot_dir, m)
+    route_str = ",".join(
+        f"{turn.value},{steps}" for turn, steps in pairwise_exclusive(route)
+    )
+    m = re.match(MOVEMENT_FUNCTION_RE, route_str + ",")
+    if not m:
+        raise RuntimeError("No movement functions found for route {route_str}")
 
-    pass
+    movement_funcs = {
+        k: v[:-1]  # Remove ending "," from each pattern
+        for k, v in m.groupdict().items()
+    }
+    main_routine = reduce(
+        lambda s, rep: s.replace(rep[1], rep[0]), movement_funcs.items(), route_str
+    )
+    mem = intcode.prg_to_memory(prg)
+    robot_output = asyncio.run(run_movement_program(mem, main_routine, movement_funcs))
+    assert robot_output[-3:-1] == [10, 10]  # Robot outputs dust value after 2 newlines
+    return robot_output[-1]
 
 
 def main(puzzle_input_f):
--- 17/test_solution.py
+++ 17/test_solution.py
@@ -1,6 +1,7 @@
 import pytest
 
-from solution import Coord, intersections, sum_alignment_parameters, grid_to_map
+import solution as s
+from solution import Coord
 
 
 @pytest.fixture
@@ -17,114 +18,85 @@ def example_grid():
 
 
 @pytest.fixture
-def example_map():
-    return {
-        Coord(0, 0): ".",
-        Coord(1, 0): ".",
-        Coord(2, 0): "#",
-        Coord(3, 0): ".",
-        Coord(4, 0): ".",
-        Coord(5, 0): ".",
-        Coord(6, 0): ".",
-        Coord(7, 0): ".",
-        Coord(8, 0): ".",
-        Coord(9, 0): ".",
-        Coord(10, 0): ".",
-        Coord(11, 0): ".",
-        Coord(12, 0): ".",
-        Coord(0, 1): ".",
-        Coord(1, 1): ".",
-        Coord(2, 1): "#",
-        Coord(3, 1): ".",
-        Coord(4, 1): ".",
-        Coord(5, 1): ".",
-        Coord(6, 1): ".",
-        Coord(7, 1): ".",
-        Coord(8, 1): ".",
-        Coord(9, 1): ".",
-        Coord(10, 1): ".",
-        Coord(11, 1): ".",
-        Coord(12, 1): ".",
-        Coord(0, 2): "#",
-        Coord(1, 2): "#",
-        Coord(2, 2): "#",
-        Coord(3, 2): "#",
-        Coord(4, 2): "#",
-        Coord(5, 2): "#",
-        Coord(6, 2): "#",
-        Coord(7, 2): ".",
-        Coord(8, 2): ".",
-        Coord(9, 2): ".",
-        Coord(10, 2): "#",
-        Coord(11, 2): "#",
-        Coord(12, 2): "#",
-        Coord(0, 3): "#",
-        Coord(1, 3): ".",
-        Coord(2, 3): "#",
-        Coord(3, 3): ".",
-        Coord(4, 3): ".",
-        Coord(5, 3): ".",
-        Coord(6, 3): "#",
-        Coord(7, 3): ".",
-        Coord(8, 3): ".",
-        Coord(9, 3): ".",
-        Coord(10, 3): "#",
-        Coord(11, 3): ".",
-        Coord(12, 3): "#",
-        Coord(0, 4): "#",
-        Coord(1, 4): "#",
-        Coord(2, 4): "#",
-        Coord(3, 4): "#",
-        Coord(4, 4): "#",
-        Coord(5, 4): "#",
-        Coord(6, 4): "#",
-        Coord(7, 4): "#",
-        Coord(8, 4): "#",
-        Coord(9, 4): "#",
-        Coord(10, 4): "#",
-        Coord(11, 4): "#",
-        Coord(12, 4): "#",
-        Coord(0, 5): ".",
-        Coord(1, 5): ".",
-        Coord(2, 5): "#",
-        Coord(3, 5): ".",
-        Coord(4, 5): ".",
-        Coord(5, 5): ".",
-        Coord(6, 5): "#",
-        Coord(7, 5): ".",
-        Coord(8, 5): ".",
-        Coord(9, 5): ".",
-        Coord(10, 5): "#",
-        Coord(11, 5): ".",
-        Coord(12, 5): ".",
-        Coord(0, 6): ".",
-        Coord(1, 6): ".",
-        Coord(2, 6): "#",
-        Coord(3, 6): "#",
-        Coord(4, 6): "#",
-        Coord(5, 6): "#",
-        Coord(6, 6): "#",
-        Coord(7, 6): ".",
-        Coord(8, 6): ".",
-        Coord(9, 6): ".",
-        Coord(10, 6): "^",
-        Coord(11, 6): ".",
-        Coord(12, 6): ".",
-    }
+def example_map(example_grid):
+    return s.grid_to_map(example_grid)
 
 
-def test_grid_to_map(example_grid, example_map):
-    assert grid_to_map(example_grid) == example_map
+class TestPartOne:
+    def test_grid_to_map(self, example_grid):
+        coords = set()
+        for y, row in enumerate(example_grid):
+            for x, val in enumerate(row):
+                coords.add(s.Coord(x, y))
+
+        m = s.grid_to_map(example_grid)
+
+        # Only expected locations are present
+        assert not set(m.keys()) - coords
+
+        # Location values match the original grid
+        for c in coords:
+            assert m[c] == example_grid[c.y][c.x]
+
+    def test_intersections(self, example_map):
+        assert list(s.intersections(example_map)) == [
+            Coord(2, 2),
+            Coord(2, 4),
+            Coord(6, 4),
+            Coord(10, 4),
+        ]
+
+    def test_sum_alignment_parameters(self, example_map):
+        assert s.sum_alignment_parameters(example_map) == 76
 
 
-def test_intersections(example_map):
-    assert list(intersections(example_map)) == [
-        Coord(2, 2),
-        Coord(2, 4),
-        Coord(6, 4),
-        Coord(10, 4),
-    ]
+@pytest.mark.parametrize(
+    "example_grid",
+    [
+        [
+            "#######...#####",
+            "#.....#...#...#",
+            "#.....#...#...#",
+            "......#...#...#",
+            "......#...###.#",
+            "......#.....#.#",
+            "^########...#.#",
+            "......#.#...#.#",
+            "......#########",
+            "........#...#..",
+            "....#########..",
+            "....#...#......",
+            "....#...#......",
+            "....#...#......",
+            "....#####......",
+        ]
+    ],
+)
+class TestPartTwo:
+    def test_find_robot(self, example_map):
+        robot_loc, robot_dir = s.find_robot(example_map)
+        assert robot_loc == Coord(0, 6)
+        assert robot_dir == s.Direction.NORTH
 
+    def test_scaffold_route(self, example_map):
+        robot_loc, robot_dir = s.find_robot(example_map)
+        scaffold_locs = {l for l, v in example_map.items() if v == "#"}
 
-def test_sum_alignement_parameters(example_map):
-    assert sum_alignment_parameters(example_map) == 76
+        # Make list for introspection
+        route = list(s.scaffold_route(robot_loc, robot_dir, example_map))
+
+        current_loc = robot_loc
+        current_dir = robot_dir
+        traversed = set()
+        for turn, steps in s.pairwise_exclusive(route):
+            if turn == s.Turn.LEFT:
+                current_dir = s.LEFT_TURN_DIRECTIONS[current_dir]
+            elif turn == s.Turn.RIGHT:
+                current_dir = s.RIGHT_TURN_DIRECTIONS[current_dir]
+            else:
+                raise ValueError(f"Unexpected {s.Turn} value {turn}")
+            for _ in range(steps):
+                current_loc += s.MOVE_DIRECTIONS[current_dir]
+                traversed.add(current_loc)
+
+        assert traversed - scaffold_locs == set()
